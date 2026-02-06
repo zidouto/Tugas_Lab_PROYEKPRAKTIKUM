84887b40b6bac2ad7793ab8b4ad9a7d6951fa45b
Graceghefira <graceghefira@gmail.com>
2026-02-06 22:47:25 +0700

    menu 3 view buku

---
+++ menu_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "data.h"
+
+void viewBuku() {
+    if(jumlahBuku == 0) {
+        printf("Belum ada data buku.\n");
+        return;
+    }
+    printf("\nDaftar Buku:\n");
+    printf("%-5s %-8s %-25s %-15s %-10s\n", "No", "Kode", "Nama", "Jenis", "Harga");
+    for(int i = 0; i < jumlahBuku; i++) {
+        printf("%-5d %-8s %-25s %-15s %.2lf\n", i+1, daftarBuku[i].kode,
+               daftarBuku[i].nama, daftarBuku[i].jenis, daftarBuku[i].harga);
+    }
+}
